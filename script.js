a282bfff367183c4a94ff96aa5e3dc75464be876
l-Aethix-l <115564288+l-Aethix-l@users.noreply.github.com>
2023-02-24 13:55:44 -0600

    who is giga ni

---
+++ script.js
@@ -0,0 +1,8 @@
+//Autoplays Music on Chrome
+var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+if (!isChrome){
+    $('#iframeAudio').remove()
+}
+else {
+    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
+}
