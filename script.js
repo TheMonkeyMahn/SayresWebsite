642ef05af41b9f1cb124bc204347feb2f93ee99f
l-Aethix-l <115564288+l-Aethix-l@users.noreply.github.com>
2023-03-07 16:12:51 -0600

    New files hehe

---
+++ script.js
@@ -0,0 +1,9 @@
+//Opens the NavBar
+function navBarOpen() {
+  document.getElementById("navbar").style.width = "100%";
+}
+
+//Closes the NavBar
+function navBarClose() {
+  document.getElementById("navbar").style.width = "0%";
+}
