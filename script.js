132d9ecb0cd255a307c006ea19ed4bd7acdb5069
l-Aethix-l <115564288+l-Aethix-l@users.noreply.github.com>
2023-03-07 16:11:33 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-//Autoplays Music on Chrome
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-if (!isChrome){
-    $('#iframeAudio').remove()
-}
-else {
-    $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-}
