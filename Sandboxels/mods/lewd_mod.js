bdde61b2b608969fcd887d5ed950765666c832f6
l-Aethix-l <115564288+l-Aethix-l@users.noreply.github.com>
2023-03-07 21:32:05 -0600

    Add files via upload

---
+++ Sandboxels/mods/lewd_mod.js
@@ -0,0 +1,24 @@
+// This is my first mod, and I couldn't think of anything else to make
+// Don't judge me
+
+elements.cum = {
+    color: "#fafafa",
+    behavior: behaviors.LIQUID,
+    category: "sex",
+    viscosity: 100000,
+    state: "liquid",
+    density: 720,
+};
+
+elements.lube = {
+    color: "#cceeff",
+    behavior: behaviors.LIQUID,
+    category: "sex",
+    viscosity: 300000,
+    state: "liquid",
+    density: 800
+};
+
+
+
+elements.human.name = "hooker"
